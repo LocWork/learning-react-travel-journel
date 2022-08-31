d5c8b7fe386e0d6bb8ff240e3b91cfc28eaac85b
LocPhan <locphanwork@gmail.com>
2022-08-31 11:13:36 +0700

    clean up code

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,14 +1,14 @@
-import React from "react"
-import "./title.css"
-import Globe from "../../img/globe.png"
+import React from 'react';
+import './title.css';
+import Globe from '../../img/globe.png';
 
-const Title =()=>{
-    return(
-        <div className="title">
-            <img src={Globe} alt="globe logo" className="tittle-icon"/>
-            <h3 className="title--text">my travel journal.</h3>
-        </div>
-    )
-}
+const Title = () => {
+  return (
+    <div className='title'>
+      <img src={Globe} alt='globe logo' className='tittle-icon' />
+      <h3 className='title--text'>My travel journal.</h3>
+    </div>
+  );
+};
 
-export default Title
+export default Title;
